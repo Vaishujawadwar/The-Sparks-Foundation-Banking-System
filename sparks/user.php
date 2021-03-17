36b38609adf579ebcf0b08a8e6656700c9fe02bd
Vaishujawadwar <80520920+Vaishujawadwar@users.noreply.github.com>
2021-03-17 17:26:02 +0530

    Update user.php

--- sparks/user.php
+++ sparks/user.php
@@ -11,30 +11,30 @@ include 'includes/common.php';
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body style="background-color:#4d4db2;">
 <?php
 include 'includes/header.php';
 ?>
 <div class="container ">
 <div class="row ">
 <br>
-<h2 class="sign text-center" style="margin-top: 100px;">CREATE</h2>
+<h2 class="sign text-center" style="margin-top: 100px; color: red;">CREATE</h2>
 <br>
 <div class="col-sm-offset-4 col-sm-6 col-md-5 ">
 <form method="POST" action="user_script.php" >
 <div class="form-group">
-<label for="name">Name</label>
+<label for="name" style="color: orange;">Name</label>
 <input type="text" class="form-control" name="name" placeholder="Name" required>
 </div>
 <div class="form-group">
-<label for="email">Email</label>
+<label for="email" style="color: orange;">Email</label>
 <input type="text" class="form-control" name="email" placeholder="Email" required>
 </div>
 <div class="form-group">
-<label for="balance">Balance</label>
+<label for="balance" style="color: orange;">Balance</label>
 <input type="number" class="form-control" name="balance" placeholder="Balance" required>
 </div>
-<button class="btn btn-danger" onclick="msg()">Submit</button>
+<button class="btn btn-danger">Submit</button>
 </form>
 </div>
 </div>
@@ -42,11 +42,6 @@ include 'includes/header.php';
 <?php
 include 'includes/footer.php';
 ?>
-<script>
-function msg(){
-  alert('User Creation Successful!!')
-}
-</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!--Latest compiled and minified JavaScript--> 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
