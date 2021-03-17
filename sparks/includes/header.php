43199efa955ba30942029cf8248941266f3dfb34
Vaishujawadwar <80520920+Vaishujawadwar@users.noreply.github.com>
2021-03-17 17:21:50 +0530

    Update header.php

--- sparks/includes/header.php
+++ sparks/includes/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-fixed-top navbar-inverse">
+<nav class="navbar navbar-fixed-top " style="background-color:#f2f2f2;">
 <div class="container">
 <div class="navbar-header">
 <button class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
@@ -6,14 +6,14 @@
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand">Sparks Bank</a>
+<a class="navbar-brand" style="color: #cc33ff;">Sparks Bank</a>
 </div>
 <div class="collapse navbar-collapse" id="mynavbar">
-<ul class="nav navbar-nav navbar-right">
-<li><a href = "index.php"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
-<li><a href="user.php"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
-<li><a href="transaction.php"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
-<li><a href="history.php"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
+<ul class="nav navbar-nav navbar-right" >
+<li><a href = "index.php" style="color: #cc33ff;"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="user.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
+<li><a href="transaction.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
+<li><a href="history.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
 </ul>
 </div>
 </div>
